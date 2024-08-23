b9f948e38ed5af7eef94a8ae800ee8d8841792a2
NabeelMS01 <nabeelmampallil123@gmail.com>
2024-08-23 21:05:57 +0530

    sdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,  // Enable the experimental `app` directory
+  },
+};
 
 export default nextConfig;
